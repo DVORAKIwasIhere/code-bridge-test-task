7a75d87fc23de3e96e79fb4616efd18234162c49
DVORAKIwasIhere <aleksey.altaev@mail.ru>
2021-11-22 21:13:32 +0800

    первая версия фильтрации с ключевыми словами, далее - поиск в описании с пониженным приоритетом к заголовку

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../hooks/redux";
@@ -13,18 +14,44 @@ export const Home = () => {
     (state) => state.ArticleReducer
   );
 
+  const [filteredState, setFilteredState] = useState(articles);
+  useEffect(() => {
+    if (articles.length) {
+      setFilteredState(articles);
+    }
+  }, [articles]);
+
+  const [searchField, setSearchField] = useState("");
+
+  const handleFilter = () => {
+    const keyWords = searchField.split(" ");
+    console.log(keyWords);
+    const filtered = articles.filter(
+      (article) => article.title.indexOf(keyWords[0]) > -1
+    );
+    setFilteredState(filtered);
+    if (keyWords === null) setFilteredState(articles);
+  };
+
   return (
     <div className="App">
+      <input
+        value={searchField}
+        onChange={(e) => {
+          setSearchField(e.target.value);
+        }}
+      />
+      <button onClick={handleFilter}>suka</button>
       {isLoading && <h1>Now is loading...</h1>}
       {error && <h1>{error}</h1>}
-      {articles &&
-        articles.map((article, index) => {
+
+      {filteredState &&
+        filteredState.map((article, index) => {
           return (
             <span key={index}>
               <div>{article.title}</div>
               <div>{article.description.substring(0, 100) + "..."}</div>
               <Link to={`/article/${index}`}>read more</Link>
-              
             </span>
           );
         })}
