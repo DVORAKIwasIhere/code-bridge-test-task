72a3f2fb85e40dbf5d2a4e3a7739e76be9480220
DVORAKIwasIhere <aleksey.altaev@mail.ru>
2021-11-26 00:18:01 +0800

    filtering title and description, next step is make its not register property, next make priority of cards, and highlight matched results

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,11 +14,11 @@ export const Home = () => {
     (state) => state.ArticleReducer
   );
 
-  const [filteredState, setFilteredState] = useState(articles);
-  
+  const [currentFilteredState, setCurrentFilteredState] = useState(articles);
+
   useEffect(() => {
     if (articles.length) {
-      setFilteredState(articles);
+      setCurrentFilteredState(articles);
     }
   }, [articles]);
 
@@ -26,26 +26,60 @@ export const Home = () => {
 
   const handleFilter = () => {
     const keyWords = searchField.split(" ");
-    
+    const filteredKeyWords = keyWords.filter((keyword)=>keyword !== '')
     // const filtered = articles.filter((article) => {
-      // return keyWords.forEach((element) => article.title.indexOf(element) > -1 );
+    // return keyWords.forEach((element) => article.title.indexOf(element) > -1 );
 
-      const filtered = articles.filter((article) => {
-        let isMatch = false
-        for (let index = 0; index < keyWords.length; index++) {
-          const element = article.title.indexOf(keyWords[index]) >-1;
-          if (element) {
-            isMatch = true
-          }      
+    const filtered = articles.filter((article) => {
+      let isMatch = false;
+      for (let index = 0; index < filteredKeyWords.length; index++) {
+        const indexTitle = article.title.indexOf(filteredKeyWords[index]) > -1;
+        const indexDescription = article.description.indexOf(filteredKeyWords[index]) > -1;
+        if (indexTitle || indexDescription) {
+          isMatch = true;
         }
-        return isMatch
       }
-    );
-    console.log(keyWords)
-    console.log(filtered)
-    setFilteredState(filtered);
-    if (keyWords.length === 0) setFilteredState(articles);
+      return isMatch;
+    });
+    // const filteredDesc = articles.filter((article) => {
+    //   let isMatch = false;
+    //   for (let index = 0; index < keyWords.length; index++) {
+    //     const element = article.description.indexOf(keyWords[index]) > -1;
+    //     if (element) {
+    //       isMatch = true;
+    //     }
+    //   }
+    //   return isMatch;
+
+    console.log(keyWords);
+    console.log(filteredKeyWords);
+    console.log(filtered);
+    setCurrentFilteredState(filtered);
+    if (keyWords.length === 0) setCurrentFilteredState(articles);
   };
+
+  // const handleFilterDesc = () => {
+  //   const keyWords = searchField.split(" ");
+  //   const filtered = articles.filter((article) => {
+  //     let isMatch = false;
+  //     for (let index = 0; index < keyWords.length; index++) {
+  //       const element = article.description.indexOf(keyWords[index]) > -1;
+  //       if (element) {
+  //         isMatch = true;
+  //       }
+  //     }
+  //     return isMatch;
+  //   });
+  //   console.log(keyWords);
+  //   console.log(filtered);
+  //   setFilteredStateDesc(filtered);
+  // };
+
+  // const handleFilter = () => {
+  //   handleFilterTitle()
+  //   // handleFilterDesc()
+  //   setCurrentFilteredState(filteredStateTitle.concat(filteredStateDesc))
+  // };
 
   return (
     <div className="App">
@@ -59,8 +93,8 @@ export const Home = () => {
       {isLoading && <h1>Now is loading...</h1>}
       {error && <h1>{error}</h1>}
 
-      {filteredState &&
-        filteredState.map((article, index) => {
+      {currentFilteredState &&
+        currentFilteredState.map((article, index) => {
           return (
             <span key={index}>
               <div>{article.title}</div>
